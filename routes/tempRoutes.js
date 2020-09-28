b6643c684543d28df0eb220ba3825acc2f220df6
EvvvB <Ballesteros.evan@gmail.com>
2020-09-28 16:18:07 -0700

    add caching for tempCsv route

--- routes/tempRoutes.js
+++ routes/tempRoutes.js
@@ -2,21 +2,30 @@ const temperature = require('../schemas/tempSchema')
 const formatDate = require('../functions/formatDate').formatDate
 
 module.exports = function(app){
-app.get('/temps/tempcsv', async function(req, res) {
-    temperature.find((err, temps)=>{
-      if(err){
-        console.log(err)
-      }else{
-        var tempToCsv = []
-  
+
+
+
+  var tempToCsv = []
+
+  temperature.find((err, temps)=>{
+    if(err){
+      console.log(err)
+    }else{
+
       temps.forEach((tempObj)=>{
         var tempArr = [formatDate(tempObj.createdAt),tempObj.temperature]
         tempToCsv.push(tempArr)
       })
-  
+      
       tempToCsv.push(['Time', 'Room'])
       tempToCsv = tempToCsv.reverse()
+      }
+    }).sort({$natural:-1}).limit(60)
+
+
   
+  app.get('/temps/tempcsv', async function(req, res) {
+   
       res.statusCode = 200;
       res.setHeader('Content-disposition', 'attachment; filename=roomTemps.csv');
       res.setHeader('Content-Type', 'text/csv');
@@ -27,16 +36,16 @@ app.get('/temps/tempcsv', async function(req, res) {
         }).toString() + '\r\n');
       });
       res.end();
-      }
-    }).sort({$natural:-1}).limit(60)
-  });
-  
+     
     
+  });
+
+
   // ROUTES FOR SENDING TEMPERATURES TO DB
   app.post("/temps/data", async(req, res)=>{
   
     console.log(req.body.pw)
-  
+    console.log(req.body.dateStr)
     if(req.body.pw == "FDAf12f*dsa36d@"){
       var newTemp = new temperature({
         temperature: req.body.temp,
@@ -48,9 +57,59 @@ app.get('/temps/tempcsv', async function(req, res) {
         console.log("ERROR")
       }else{
       console.log("save successful!")
-      }
+
+      var tempArr = [req.body.dateStr, req.body.temp]
+      tempToCsv.splice(1,1)
+      tempToCsv.push(tempArr)
+      console.log(tempToCsv)
+    }
     }) 
   }
   res.end()
   })
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/temps/tempcsv', async function(req, res) {
+//     temperature.find((err, temps)=>{
+//       if(err){
+//         console.log(err)
+//       }else{
+//         var tempToCsv = []
+  
+//       temps.forEach((tempObj)=>{
+//         var tempArr = [formatDate(tempObj.createdAt),tempObj.temperature]
+//         tempToCsv.push(tempArr)
+//       })
+  
+//       tempToCsv.push(['Time', 'Room'])
+//       tempToCsv = tempToCsv.reverse()
+  
+//       res.statusCode = 200;
+//       res.setHeader('Content-disposition', 'attachment; filename=roomTemps.csv');
+//       res.setHeader('Content-Type', 'text/csv');
+      
+//       tempToCsv.forEach(function(item) {
+//         res.write(item.map(function(field) {
+//           return field.toString();
+//         }).toString() + '\r\n');
+//       });
+//       res.end();
+//       }
+//     }).sort({$natural:-1}).limit(60)
+//   });
+
+
+
+
 }
+
+
